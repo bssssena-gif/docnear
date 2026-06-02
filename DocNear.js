5c1b2710b8365a6e496c3852349efaff916f0288
bssssena-gif <bssssena@gmail.com>
2026-06-02 19:55:04 +0530

    Add functions to show messages for various databases

---
+++ DocNear.js
@@ -0,0 +1,19 @@
+function showMessage(msg){
+  alert(msg);
+}
+
+function openDoctors(){
+  showMessage("Doctors Database Coming Soon");
+}
+
+function openMedicalStores(){
+  showMessage("Medical Stores Database Coming Soon");
+}
+
+function openAmbulance(){
+  showMessage("Ambulance Database Coming Soon");
+}
+
+function openLabs(){
+  showMessage("Labs Database Coming Soon");
+}
